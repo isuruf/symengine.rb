439eb624de3562eb6842423622b5fd6549869b84
Isuru Fernando <isuruf@gmail.com>
2016-10-08 18:28:32 +0530

    rubocop conda changes

--- ext/symengine/extconf.rb
+++ ext/symengine/extconf.rb
@@ -2,43 +2,47 @@ require 'rbconfig'
 
 ruby_executable = File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['RUBY_INSTALL_NAME'] + RbConfig::CONFIG['EXEEXT'])
 
-generator = ""
+generator = ''
 
-if (RbConfig::CONFIG['host_os'] =~ /mswin|mingw/)
-    generator = '-G "MSYS Makefiles"'
+if RbConfig::CONFIG['host_os'] =~ /mswin|mingw/
+  generator = '-G "MSYS Makefiles"'
 end
 
 is_windows = (RbConfig::CONFIG['host_os'] =~ /mswin|mingw/)
 
 # Check if cmake is installed
-cmake_found = system("cmake --version")
-cmake = "cmake"
-if not cmake_found
-    # Install cmake using conda
-    require 'conda'
-    Conda.add_channel("conda-forge")
-    Conda.add("cmake")
-    cmake = File.join(Conda.SCRIPTDIR, "cmake")
+cmake_found = system('cmake --version')
+cmake = 'cmake'
+unless cmake_found
+  # Install cmake using conda
+  require 'conda'
+  Conda.add_channel('conda-forge')
+  Conda.add('cmake')
+  cmake = File.join(Conda.SCRIPTDIR, 'cmake')
 end
 
-args = ARGV.join(" ")
+args = ARGV.join(' ')
+dir = File.dirname(File.dirname(__dir__))
 
-cmd = "#{cmake} #{generator} -DCMAKE_INSTALL_PREFIX=../../ -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=yes -DRUBY_EXECUTABLE=#{ruby_executable} #{args} ../../ "
+cmd = "#{cmake} #{generator} "\
+      "-DCMAKE_INSTALL_PREFIX=#{dir} "\
+      '-DCMAKE_INSTALL_RPATH_USE_LINK_PATH=yes '\
+      "-DRUBY_EXECUTABLE=#{ruby_executable} "\
+      "#{args} #{dir}"
+
 symengine_found = system(cmd)
 
-if not symengine_found
-    # Install symengine using conda and rerun
-    require 'conda'
-    Conda.add_channel("conda-forge")
-    Conda.add_channel("symengine")
-    Conda.add("symengine")
-    if is_windows
-        libdir = File.join(Conda::PREFIX, "Library", "CMake")
-    else
-        libdir = File.join(Conda::PREFIX, "lib", "cmake")
-    end
-    cmd = cmd + "-DSymEngine_DIR=#{libdir}"
-    if not system(cmd)
-        raise "Something went wrong with configuring symengine gem"
-    end
+unless symengine_found
+  # Install symengine using conda and rerun
+  require 'conda'
+  Conda.add_channel('conda-forge')
+  Conda.add_channel('symengine')
+  Conda.add('symengine')
+  libdir = if is_windows
+             File.join(Conda::PREFIX, 'Library', 'CMake')
+           else
+             File.join(Conda::PREFIX, 'lib', 'cmake')
+           end
+  cmd += "-DSymEngine_DIR=#{libdir}"
+  raise 'Something went wrong with configuring symengine gem' unless system(cmd)
 end
